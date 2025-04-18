ebaeb5a70bfa6e9435f892d024583155733944f6
AnnaBurg <any.burganova@gmail.com>
2025-04-18 15:14:36 +0400

    регистр

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 # @app.route('/index')
 def index():
-    render_template('registration.html')
+    return render_template('registration.html')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
