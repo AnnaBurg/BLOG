14e4a25ae7bb8e19418df1eec385e886b6324186
AnnaBurg <any.burganova@gmail.com>
2025-04-22 15:49:38 +0400

    регистр и логин

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route('/registration')
 def register():
     return render_template('registration.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
